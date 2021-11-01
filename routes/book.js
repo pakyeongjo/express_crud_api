e9dcdf9ebda21a030d26dd49867118e56226834b
pakyeongjo <pakyeongjo@gmail.com>
2021-11-01 17:45:03 +0900

    add get route for book

--- routes/book.js
+++ routes/book.js
@@ -1,8 +1,29 @@
 var express = require('express');
+const db = require('../models/index');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.json({value: NaN});
+router.get('/', async(req, res, next) => {
+  let result = await findAllFromBooks();
+  res.json(result);
 });
+
+router.get('/:id', async(req, res, next) => {
+  let result = await findOneFromBooks(req.params['id']);
+  res.json(result)
+});
+
+async function findAllFromBooks() {
+  const books = await db.Book.findAll({raw: true});  
+  return books;
+}
+
+async function findOneFromBooks(id) {
+  const book = await db.Book.findAll({
+    where: {
+      id: id
+    },
+    raw: true});  
+  return book;
+}
 
 module.exports = router;
